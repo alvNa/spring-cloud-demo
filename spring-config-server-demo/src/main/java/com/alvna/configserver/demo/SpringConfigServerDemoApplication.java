f3e53efeb2ff567293234637fd7aaae15ebfbc12
alvNa <anavarro@datiobd.com>
2021-07-22 00:07:59 +0200

    changes

---
+++ spring-config-server-demo/src/main/java/com/alvna/configserver/demo/SpringConfigServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alvna.configserver.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigServerDemoApplication.class, args);
+	}
+
+}
